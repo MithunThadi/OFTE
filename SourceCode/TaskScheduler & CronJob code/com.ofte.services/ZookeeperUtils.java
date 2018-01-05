c71e905a228ace700006caed57c4b4050a1d3f8d
SampathPotnuru <32093467+SampathPotnuru@users.noreply.github.com>
2018-01-05 23:01:54 +0530

    scheduled & cron job updated

---
+++ SourceCode/TaskScheduler & CronJob code/com.ofte.services/ZookeeperUtils.java
@@ -0,0 +1,11 @@
+package com.ofte.services;
+
+import java.util.Random;
+
+public class ZookeeperUtils {
+	public static int portGenerator() {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((9999 - 1) + 1) + 1;
+		return randomNum;
+	}
+}
